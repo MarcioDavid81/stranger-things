9049cadc40f7b110612a5978a19457cad5239399
marciodavid <marciodavid81@gmail.com>
2026-01-31 01:54:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText)
+
+ScrollSmoother.create({
+  smooth: 3,
+  effects: true
+})
+
+gsap.from(
+  ".hero",
+  {
+    opacity: 0,
+    duration: 1,
+  }
+)
+gsap.from(
+  "picture:nth-child(1)",
+  {
+    y: -60,
+    duration: 1,
+  }
+)
+gsap.from(
+  "picture:nth-child(2)",
+  {
+    y: 60,
+    duration: 1,
+  }
+)
+
+gsap.from(
+  ".card",
+  {
+    opacity: 0,
+    y: 30,
+    filter: "blur(10px)",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".cards",
+      start: "0% 70%",
+      end: "100% 70%",
+      scrub: true,
+    }
+  }
+)
+
+gsap.from(
+  ".secaoObrigado ul li",
+  {
+    opacity: 0,
+    x: 40,
+    filter: "blur(10px)",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".secaoObrigado ul",
+      start: "0% 80%",
+      end: "100% 50%",
+      scrub: true,
+    }
+  }
+)
+
+gsap.from(
+  "footer",
+  {
+    y: "-30%",
+    immediateRender: false,
+    scrollTrigger: {
+      trigger: "footer",
+      scrub: true,
+      invalidateOnRefresh: true,
+      start: "0% 100%",
+      end: "100% 100%",
+    }
+  }
+)
+
+// const split = SplitText.create(".textoSplit", {
+//     type: "lines, words, chars",
+//     mask: "lines",
+//   });
+
+// gspap.from(split.chars, {
+//     y: 40,
+//     duration: 0.3,
+//     stagger: 0.05,
+//   });
+
+SplitText.create(".textoSplit", {
+  type: "lines, words, chars",
+  mask: "lines",
+  onSplit(self) {
+    return gsap.from(self.chars, {
+      y: 40, 
+      opacity: 0,
+      duration: 0.3,  
+      stagger: 0.05
+    });
+  }
+});
